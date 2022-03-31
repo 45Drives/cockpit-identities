21d5f57942e29bcf796e52425f4c13e875f0ca02
joshuaboud <joshuaboud@hotmail.com>
2022-03-31 12:49:07 -0300

    start ssh keys

--- users/src/components/SSHKeys.vue
+++ users/src/components/SSHKeys.vue
@@ -1,52 +1,49 @@
 <template>
-	<div>
-		<label class="block text-sm font-medium">SSH Keys</label>
-		<div class="mt-8 flex flex-col overflow-visible">
-			<div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8 overflow-visible">
-				<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8 overflow-visible">
-					<div
-						class="overflow-visible shadow ring-1 ring-black ring-opacity-5 dark:ring-gray-700 md:rounded-lg"
-					>
-						<table class="min-w-full divide-y divide-gray-300 dark:divide-gray-700 overflow-visible">
-							<thead class="bg-neutral-50 dark:bg-neutral-800 overflow-visible">
-								<tr class="overflow-visible">
-									<th
-										scope="col"
-										class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-6 lg:pl-8"
-									>{{ user.name === "" ? user.user : user.name }}'s Keys</th>
-									<th
-										scope="col"
-										class="sr-only py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-6 lg:pl-8"
-									>Remove</th>
-									<div class="relative">
-										<PlusIcon
-											@click="showModal('add')"
-											class="w-5 h-5 absolute right-3 top-3.5 cursor-pointer text-gray-500"
-										/>
-									</div>
-								</tr>
-							</thead>
-							<tbody class="dark:bg-neutral-800">
-								<tr
-									v-for="(key, index) in keys"
-									:class="index % 2 === 0 ? undefined : 'bg-neutral-50 dark:bg-neutral-700'"
-								>
-									<td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6 lg:pl-8">{{ key }}</td>
-									<td class="flex flex-row justify-end whitespace-nowrap py-4 pl-3 pr-4 text-sm font-medium">
-										<MinusIcon
-											@click="showModal('remove', key)"
-											class="uppercase text-red-600 hover:text-red-900 cursor-pointer w-5 h-5"
-										/>
-									</td>
-								</tr>
-								<tr v-if="keys.length === 0">
-									<td
-										class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-500 sm:pl-6 lg:pl-8"
-									>No keys. Click "+" to add one.</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
+	<div class="mt-1 flex flex-col overflow-visible">
+		<div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8 overflow-visible">
+			<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8 overflow-visible">
+				<div
+					class="overflow-visible shadow ring-1 ring-black ring-opacity-5 dark:ring-gray-700 md:rounded-lg"
+				>
+					<table class="min-w-full divide-y divide-gray-300 dark:divide-gray-700 overflow-visible">
+						<thead class="bg-neutral-50 dark:bg-neutral-800 overflow-visible">
+							<tr class="overflow-visible">
+								<th
+									scope="col"
+									class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-6 lg:pl-8"
+								>{{ user.name === "" ? user.user : user.name }}'s Authorized Public SSH Keys</th>
+								<th
+									scope="col"
+									class="sr-only py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-6 lg:pl-8"
+								>Remove</th>
+								<div class="relative">
+									<PlusIcon
+										@click="showModal('add')"
+										class="w-5 h-5 absolute right-3 top-3.5 cursor-pointer text-gray-500"
+									/>
+								</div>
+							</tr>
+						</thead>
+						<tbody class="dark:bg-neutral-800">
+							<tr
+								v-for="(key, index) in keys"
+								:class="index % 2 === 0 ? undefined : 'bg-neutral-50 dark:bg-neutral-700'"
+							>
+								<td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium sm:pl-6 lg:pl-8">{{ key }}</td>
+								<td class="flex flex-row justify-end whitespace-nowrap py-4 pl-3 pr-4 text-sm font-medium">
+									<MinusIcon
+										@click="showModal('remove', key)"
+										class="uppercase text-red-600 hover:text-red-900 cursor-pointer w-5 h-5"
+									/>
+								</td>
+							</tr>
+							<tr v-if="keys.length === 0">
+								<td
+									class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-500 sm:pl-6 lg:pl-8"
+								>No keys. Click "+" to add one.</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
@@ -54,7 +51,9 @@
 </template>
 
 <script>
+import { ref, reactive, watch } from 'vue';
 import { useSpawn } from '../hooks/useSpawn';
+import { MinusIcon, PlusIcon } from '@heroicons/vue/solid';
 const authorizedKeysSyntax = {
 	parse: (string) => {
 		return string.split('\n')
@@ -111,12 +110,34 @@ export default {
 		const keys = ref([]);
 		const newKey = reactive({});
 
-		const authorizedKeysFile = cockpit.file(`${user.home}/.ssh/authorized_keys`, { superuser: 'try' });
+		const authorizedKeysFile = cockpit.file(`${props.user.home}/.ssh/authorized_keys`, { superuser: 'try' });
 
 		let modalConfirmCallback = () => { }
 
 		const getKeys = async () => {
-			let tmpKeys = [];
+			let tmpKeys = [
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+				'asdfasdfa',
+			];
 			try {
 
 			} catch (error) {
@@ -143,6 +164,10 @@ export default {
 			modalConfirmCallback,
 			showModal,
 		}
+	},
+	components: {
+		PlusIcon,
+		MinusIcon,
 	}
 }
 </script>
