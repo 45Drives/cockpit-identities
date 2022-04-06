62ce5d27f5c38bcb2d844cc98590b5dd4bd6ebc5
joshuaboud <joshuaboud@hotmail.com>
2022-04-06 16:22:07 -0300

    use symbols instead of strings for inject

---
+++ users/src/keys.js
@@ -0,0 +1,20 @@
+/**
+ * Notifications component instance to add notifs
+ */
+export const notificationsInjectionKey = Symbol();
+/**
+ * ref to boolean for dark mode (can't set dark mode by changing)
+ */
+export const darkModeInjectionKey = Symbol();
+/**
+ * ref to boolean to show loading spinners
+ */
+export const processingInjectionKey = Symbol();
+/**
+ * ref to list of shells (shellObj)
+ */
+export const shellsInjectionKey = Symbol();
+/**
+ * ref to list of groups
+ */
+export const groupsInjectionKey = Symbol();
