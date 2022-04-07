2b8355ba60cbcf57c25008cd24fde661dbe9ccfc
joshuaboud <joshuaboud@hotmail.com>
2022-04-07 17:02:23 -0300

    add expiry

--- users/src/components/UserPassword.vue
+++ users/src/components/UserPassword.vue
@@ -1,27 +1,37 @@
 <template>
 	<div v-if="!modalOnly">
 		<label class="block text-sm font-medium">User Login</label>
-		<div class="flex flex-row space-x-3 mt-1">
+		<div class="flex flex-row space-x-3 mt-1 items-baseline">
 			<button
 				class="btn btn-primary"
-				@click="userPassword.showModal = true"
+				@click="setPassword"
 			>{{ userPassword.isSet ? 'Change' : 'Set' }} Account Password</button>
+			<button class="btn btn-primary" @click="togglePasswordLock()" v-if="userPassword.isSet">
+				<div class="flex flex-row items-center">
+					<span class="mr-2">{{ userPassword.isLocked ? 'Unlock' : 'Lock' }} Account Password</span>
+					<LockClosedIcon v-if="userPassword.isLocked" class="w-5 h-5" />
+					<LockOpenIcon v-else class="w-5 h-5" />
+				</div>
+			</button>
 			<button
 				class="btn btn-primary"
-				@click="showModal('remove')"
+				@click="showExpirePasswordModal = true"
 				v-if="userPassword.isSet"
-			>Remove Account Password</button>
+				:disabled="userPassword.isExpired"
+			>Force Expire Account Password</button>
+			<div v-if="userPassword.isSet" class="text-gray-500">{{ userPassword.expires }}</div>
 		</div>
 		<ModalPopup
-			:showModal="showRemovePasswordModal"
-			:onApply="removePassword"
-			:onCancel="showRemovePasswordModal = false"
+			:showModal="showExpirePasswordModal"
+			:onApply="expirePassword"
+			:onCancel="() => showExpirePasswordModal = false"
+			:headerText="`Expire password for ${user}?`"
 			applyText="Yes"
 			cancelText="No"
 		>
 			<div class="flex flex-row">
 				<ExclamationCircleIcon class="w-5 h-5 mr-2 text-red-600" />
-				<span>Remove password for {{ user }}? This cannot be undone.</span>
+				<span>They will need to set a new password at next login.</span>
 			</div>
 		</ModalPopup>
 	</div>
@@ -73,7 +83,8 @@
 
 <script>
 import ModalPopup from "./ModalPopup.vue";
-import { ExclamationCircleIcon, CheckIcon, XIcon } from '@heroicons/vue/solid';
+import { ExclamationCircleIcon, CheckIcon, XIcon, LockClosedIcon, LockOpenIcon } from '@heroicons/vue/solid';
+// import { LockClosedIcon, LockOpenIcon } from "@heroicons/vue/outline";
 import { ref, reactive, watch, inject } from 'vue';
 import { useSpawn, errorString, errorStringHTML } from '../hooks/useSpawn';
 import { notificationsInjectionKey, processingInjectionKey } from "../keys";
@@ -101,6 +112,9 @@ export default {
 			applyCallback: () => userPassword.showModal = false,
 			cancelCallback: () => userPassword.showModal = false,
 			isSet: false,
+			isLocked: false,
+			expires: 'never',
+			isExpired: false,
 			allRequirementsSatisfied: false,
 			requirements: [
 				{
@@ -137,9 +151,48 @@ export default {
 			check: new RegExp(`^(?!${props.user}$).*$`),
 		};
 		userPassword.requirements.push(userPasswordRequirementNotSameAsUser);
-		const showRemovePasswordModal = ref(false);
+		const showExpirePasswordModal = ref(false);
 		const processing = inject(processingInjectionKey);
 		const notifications = inject(notificationsInjectionKey).value;
+
+		const checkPasswdStatus = async () => {
+			try {
+				const passwdStatusFields = (await useSpawn(['passwd', '--status', props.user], { superuser: 'try' }).promise()).stdout
+					.trim().split(' ');
+				switch (passwdStatusFields[1]) {
+					case 'P':
+						userPassword.isSet = true;
+						userPassword.isLocked = false;
+						break;
+					case 'NP':
+						userPassword.isSet = false;
+						userPassword.isLocked = false;
+						break;
+					case 'L':
+						userPassword.isLocked = true;
+						break;
+					default:
+						throw new Error(`Unknown field in passwd -S: ${passwdStatusFields[1]}. Should be NP, P, or L.`);
+				}
+				const chageLines = (await useSpawn(['chage', '-l', props.user], { superuser: 'try' }).promise()).stdout
+					.split('\n'); // split lines
+				console.log(chageLines);
+				const chageExpires = chageLines.find(line => /^Password expires/.test(line))
+					.split(':')[1]
+					.trim();
+				userPassword.isExpired = chageExpires === "password must be changed";
+				userPassword.expires = chageExpires.replace(/^(?!password must be changed)(?!never)(.*)$/, "Password expires on $1.")
+					.replace("password must be changed", "Password is expired.")
+					.replace("never", "Password never expires.");
+			} catch (state) {
+				notifications.constructNotification(
+					"Failed to check password status",
+					`${errorStringHTML(state)}\nPretending it is not set.`,
+					'warning'
+				);
+				userPassword.isSet = false;
+			}
+		};
 
 		const setPassword = async () => {
 			let watchStopHandle = null;
@@ -176,6 +229,7 @@ export default {
 					state.proc.input(`${userPassword.pass1}\n${userPassword.pass2}\n`);
 					await state.promise();
 					notifications.constructNotification(`Set password for ${props.user}`, "Password was set successfully.", 'success');
+					await checkPasswdStatus();
 				} catch (state) {
 					notifications.constructNotification(
 						"Error setting password",
@@ -192,23 +246,39 @@ export default {
 			userPassword.showModal = false;
 		};
 
-		const removePassword = async () => {
-			console.log("remove password");
-		};
-
-		const checkIfSet = async () => {
+		const expirePassword = async () => {
 			try {
-				const passwdStatus = (await useSpawn(['passwd', '--status', props.user], { superuser: 'try' }).promise()).stdout.trim();
-				userPassword.isSet = (passwdStatus.split(' ')[1] === 'P');
+				await useSpawn(['passwd', '-e', props.user], { superuser: 'try' }).promise();
+				await checkPasswdStatus();
 			} catch (state) {
 				notifications.constructNotification(
-					"Failed to check if password is already set",
-					`${errorStringHTML(state)}\nPretending it is not set.`,
+					`Failed to expire password`,
+					errorStringHTML(state),
 					'warning'
 				);
-				userPassword.isSet = false;
 			}
-		}
+			showExpirePasswordModal.value = false;
+		};
+
+		const togglePasswordLock = async () => {
+			const argv = ['passwd'];
+			if (userPassword.isLocked) {
+				argv.push('-u');
+			} else {
+				argv.push('-l');
+			}
+			argv.push(props.user);
+			try {
+				await useSpawn(argv, { superuser: 'try' }).promise();
+				await checkPasswdStatus();
+			} catch (state) {
+				notifications.constructNotification(
+					`Failed to ${userPassword.isLocked ? 'un' : ''}lock password`,
+					errorStringHTML(state),
+					'warning'
+				);
+			}
+		};
 
 		watch([() => userPassword.pass1, () => userPassword.pass2], () => {
 			userPassword.valid = false;
@@ -223,16 +293,17 @@ export default {
 			userPassword.allRequirementsSatisfied = !userPassword.requirements.map(req => req.satisfied = req.check.test(userPassword.pass1)).includes(false);
 		});
 
-		watch(() => props.user, () => {
+		watch(() => props.user, async () => {
 			userPasswordRequirementNotSameAsUser.check = new RegExp(`^(?!${props.user}$).*$`);
-			checkIfSet();
+			await checkPasswdStatus();
 		}, { immediate: !props.newUser });
 
 		return {
 			userPassword,
-			showRemovePasswordModal,
+			showExpirePasswordModal,
 			setPassword,
-			removePassword,
+			expirePassword,
+			togglePasswordLock,
 		}
 	},
 	components: {
@@ -240,6 +311,8 @@ export default {
 		ExclamationCircleIcon,
 		CheckIcon,
 		XIcon,
+		LockClosedIcon,
+		LockOpenIcon,
 	},
 }
 </script>
