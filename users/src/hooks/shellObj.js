7fb5b6b02a5b198cc72e3d0cde02ed9cda237cb4
joshuaboud <joshuaboud@hotmail.com>
2022-04-07 12:03:09 -0300

    set isShellObj property

--- users/src/hooks/shellObj.js
+++ users/src/hooks/shellObj.js
@@ -5,5 +5,5 @@ function shellObj(path) {
 		.replace(/-/g, ' ') // kebab case to spaces
 		.replace(/\b\w/g, c => c.toUpperCase()) // capitalize first letters
 		.replace(/Nologin/, "No Login"); // special case of nologin
-	return { path, name };
+	return { path, name, isShellObj: true };
 }
