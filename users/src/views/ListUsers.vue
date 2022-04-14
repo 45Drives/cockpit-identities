08736c008646a9a26b2c9eb93c085aa0c3f1913c
joshuaboud <joshuaboud@hotmail.com>
2022-04-14 10:18:10 -0300

    clean up code and show badges for logged in user and root

--- users/src/views/ListUsers.vue
+++ users/src/views/ListUsers.vue
@@ -1,11 +1,18 @@
 <template>
 	<div class="flex flex-row flex-wrap content-start gap-well p-well w-full">
-		<button
-			v-for="user in users"
-			@click="openUser(user.user)"
-			class="card"
-		>
-			<div class="card-body flex flex-col items-center justify-center h-36 min-w-[10rem]">
+		<button v-for="user in users" @click="openUser(user.user)" class="card">
+			<div class="card-body flex flex-col items-center justify-center h-36 min-w-[10rem] relative">
+				<div class="flex flex-col gap-2 absolute right-3 top-3">
+					<StarIcon
+						v-if="user.currentLoggedIn"
+						class="size-icon icon-default"
+						title="You"
+					/>
+					<ShieldExclamationIcon
+						v-if="user.uid === 0"
+						class="size-icon icon-default"
+					/>
+				</div>
 				<UserIcon class="w-20 h-20 icon-default shrink-0" />
 				<div>{{ user.name ?? user.user }}</div>
 				<div
@@ -14,10 +21,7 @@
 				>({{ user.user }})</div>
 			</div>
 		</button>
-		<button
-			@click="addUser()"
-			class="card"
-		>
+		<button @click="addUser()" class="card">
 			<div class="card-body flex flex-col items-center justify-center h-36 min-w-[10rem]">
 				<UserAddIcon class="w-20 h-20 icon-default shrink-0" />
 				<div>New User</div>
@@ -27,7 +31,7 @@
 </template>
 
 <script>
-import { UserIcon, UserAddIcon } from "@heroicons/vue/solid";
+import { UserIcon, UserAddIcon, StarIcon, ShieldExclamationIcon } from "@heroicons/vue/solid";
 import { useSpawn, errorString } from '../hooks/useSpawn';
 import { ref } from "vue";
 
@@ -37,19 +41,26 @@ export default {
 
 		const getUsers = async () => {
 			try {
-				const passwdDB = (await useSpawn(['getent', 'passwd'], { superuser: 'try' }).promise()).stdout;
-				passwdDB.split('\n').forEach(record => {
-					if (/^\s*$/.test(record))
-						return;
-					const fields = record.split(':');
-					const uid = fields[2];
-					const uidInt = parseInt(uid);
-					if (uidInt < 1000 && uidInt !== 0)
-						return;
-					const user = fields[0];
-					const name = fields[4];
-					users.value.push({ user, name: name === "" ? user : name });
-				})
+				const currentLoggedInUser = (await cockpit.user()).name;
+				users.value = (await useSpawn(['getent', 'passwd'], { superuser: 'try' }).promise()).stdout
+					.split('\n')
+					.map(record => {
+						if (/^\s*$/.test(record))
+							return null;
+						const fields = record.split(':');
+						const uid = fields[2];
+						const uidInt = parseInt(uid);
+						if (uidInt < 1000 && uidInt !== 0)
+							return null;
+						const user = fields[0];
+						const name = fields[4];
+						return {
+							user,
+							name: name === "" ? user : name,
+							currentLoggedIn: user === currentLoggedInUser,
+							uid: uidInt,
+						};
+					}).filter(user => user !== null) ?? [];
 			} catch (state) {
 				alert("Failed to get users: " + errorString(state));
 			}
@@ -74,6 +85,8 @@ export default {
 	components: {
 		UserIcon,
 		UserAddIcon,
+		StarIcon,
+		ShieldExclamationIcon,
 	}
 }
 </script>
