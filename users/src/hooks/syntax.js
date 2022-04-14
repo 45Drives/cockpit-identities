110d36c2d5a1b0959ab762aaea396d114d560f98
joshuaboud <joshuaboud@hotmail.com>
2022-04-14 10:18:32 -0300

    fix getting fingerprint

--- users/src/hooks/syntax.js
+++ users/src/hooks/syntax.js
@@ -1,26 +1,33 @@
 import { useSpawn, errorString } from './useSpawn';
 
+async function getFingerprint(pubKey) {
+	let tmpFile, result;
+	try {
+		tmpFile = (await useSpawn(['mktemp'], { superuser: 'try' }).promise()).stdout.trim();
+		const ddState = useSpawn(['dd', `of=${tmpFile}`], { superuser: 'try' });
+		ddState.proc.input(pubKey);
+		await ddState.promise();
+		result = (await useSpawn(['ssh-keygen', '-l', '-f', tmpFile], { superuser: 'try' }).promise()).stdout
+			.split(' ')[1]
+	} catch (state) {
+		result = "Failed to get fingerprint: " + errorString(state);
+	} finally {
+		if (tmpFile)
+			useSpawn(['rm', tmpFile], { superuser: 'try' });
+	}
+	return result;
+}
+
 const SSHAuthorizedKeysSyntax = {
-	parse: (string) => {
-		return string.split('\n')
+	parse: async (string) => {
+		return (await Promise.all(string.split('\n')
 			.filter(line => !/^(\s*|\s*#.*)$/.test(line)) // remove empty lines and comments
 			.map(line => line.replace(/\s*#.*$/, '')) // remove end-of-line comments
 			.map(async (line) => {
-				const obj = { pubKey: line };
-				let tmpFile;
-				try {
-					tmpFile = (await useSpawn(['mktemp'], { superuser: 'try' }).promise()).stdout.trim();
-					const ddState = useSpawn(['dd', `of=${tmpFile}`], { superuser: 'try' });
-					ddState.proc.input(line);
-					await ddState.promise();
-					obj.fingerprint = (await useSpawn(['ssh-keygen', '-l', '-f', tmpFile], { superuser: 'try' }).promise()).stdout
-						.split(' ')[1]
-				} catch (state) {
-					obj.fingerprint = "Failed to get fingerprint: " + errorString(state);
-				} finally {
-					if (tmpFile)
-						useSpawn(['rm', tmpFile], { superuser: 'try' });
-				}
+				const obj = {
+					pubKey: line,
+					fingerprint: await getFingerprint(line),
+				};
 				const regexp = new RegExp([
 					/^((?:\w+=(?:"[^"]*"|'[^']*'|(?:[^,\s]|\\\s)*),?)*)/, // first group: possible options at start of line
 					/\s*/,                                                // space delimiter
@@ -40,8 +47,8 @@ const SSHAuthorizedKeysSyntax = {
 				obj.algo = regMatch[2];
 				obj.pubKey = regMatch[3];
 				obj.comment = regMatch[4];
-				return { ...obj };
-			})
+				return obj;
+			})))
 			.filter(obj => obj !== null);
 	},
 	stringify: (objs) => {
