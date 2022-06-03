ef2bee84cf37472b834dde8e18324254aef2568a
joshuaboud <joshuaboud@hotmail.com>
2022-06-03 17:29:58 -0300

    add copyright disclaimers

--- identities/src/components/UserPassword.vue
+++ identities/src/components/UserPassword.vue
@@ -1,3 +1,20 @@
+<!--
+Copyright (C) 2022 Josh Boudreau <jboudreau@45drives.com>
+
+This file is part of Cockpit Identities.
+
+Cockpit Identities is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+
+Cockpit Identities is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Cockpit Identities.
+If not, see <https://www.gnu.org/licenses/>. 
+-->
+
 <template>
 	<div v-if="!modalOnly && allowed">
 		<label class="block text-label">User Login</label>
